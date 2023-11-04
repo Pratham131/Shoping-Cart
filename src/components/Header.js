1b7c3053800fa255fb88ac0959b805ab43f48291
Pratham131 <prathamakhand2000@gmail.com>
2023-11-05 00:53:06 +0530

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h3>Shopping Cart</h3>
+
+      <div className='leftDiv'>
+        <p>Home Page</p>
+        <p>Cart Page</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
